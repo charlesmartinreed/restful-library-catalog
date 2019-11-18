4a7b4764e141a2c570b4d493435887113667a595
charlesmartinreed <charlesmartinreed@icloud.com>
2019-11-18 16:00:46 -0600

    Add mongoose package.

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+  // get all the variables from our env file and expose them, when in local development mode
+  require("dotenv").config();
+}
+
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
@@ -18,6 +23,19 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+/*
+======================
+MONGODB/MONGOOSE SETUP
+======================
+*/
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Now connected to Mongoose"));
 
 /*
 ======================
